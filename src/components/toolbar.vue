c59bdf550480aa396b7561d9aa8fa3f0058d7bf8
daixu1 <daixu1@vipkid.com.cn>
2020-04-04 02:19:01 +0800

    arrow

--- src/components/toolbar.vue
+++ src/components/toolbar.vue
@@ -1,36 +1,39 @@
 <template>
   <div class="kt-container">
-    <aside class="kt-tools">
-      <ul class="kt-selector-list">
-        <template v-for="(selector, i) in tools">
-          <li :key="i + 'li'" 
-          :class="[
-            'kt-selector', 'kt-icon-'+ selector, {'graph-checked': i === 4}
-          ]"></li>
-          <popper-graph v-if="i === 4" :key="i + 'popper'"></popper-graph>
-        </template>
-      </ul>
-      <i class="kt-separator"></i>
-    </aside>
-    <div class="kt-settings">
-      <board-style></board-style>
-      <stroke-width></stroke-width>
-      <line-style></line-style>
-      <eraser-width></eraser-width>
-      <graph-style></graph-style>
-      <stroke-color></stroke-color>
+    <div class="kt-main" v-show="visible">
+      <aside class="kt-tools">
+        <ul class="kt-selector-list">
+          <template v-for="(selector, i) in tools">
+            <li :key="i + 'li'" 
+            :class="[
+              'kt-selector', 'kt-icon-'+ selector, {'graph-checked': i === 4}
+            ]"></li>
+            <popper-graph v-if="i === 4" :key="i + 'popper'"></popper-graph>
+          </template>
+        </ul>
+        <i class="kt-separator"></i>
+      </aside>
+      <div class="kt-settings">
+        <board-style></board-style>
+        <stroke-width></stroke-width>
+        <line-style></line-style>
+        <eraser-width></eraser-width>
+        <graph-style></graph-style>
+        <stroke-color></stroke-color>
+      </div>
+      <aside class="kt-operations">
+        <i class="kt-separator"></i>
+        <ul class="kt-selector-list">
+          <template v-for="(operation, i) in operations">
+            <li :key="i" :class="['kt-selector', 'kt-icon-' + operation]"></li>
+            <i class="kt-separator" v-if="i === 3" :key="i+'separator'"></i>
+          </template>
+          <popper-countdown></popper-countdown>
+        </ul>
+      </aside>
     </div>
-    <aside class="kt-operations">
-      <i class="kt-separator"></i>
-      <ul class="kt-selector-list">
-        <template v-for="(operation, i) in operations">
-          <li :key="i" :class="['kt-selector', 'kt-icon-' + operation]"></li>
-          <i class="kt-separator" v-if="i === 3" :key="i"></i>
-        </template>
-        <popper-countdown></popper-countdown>
-      </ul>
-    </aside>
-    <!-- <board></board> -->
+    <i :class="['kt-arrow', 'kt-arrow-' + (visible ? 'collapse' : 'expand')]"
+    @click="handleArrowClick"></i>
   </div>
 </template>
 
@@ -61,8 +64,14 @@ export default {
   },
   data () {
     return {
+      visible: true,
       tools: ['mouse', 'brush', 'eraser', 'line', 'square', 'whiteboard'],
       operations: ['remove', 'clear', 'undo', 'redo', 'reset']
+    }
+  },
+  methods: {
+    handleArrowClick () {
+      this.visible = !this.visible
     }
   }
 }
